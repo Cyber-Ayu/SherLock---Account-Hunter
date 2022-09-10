e7ae778988933e431e5cf825012c2dc286f0734e
Ayush Soni <ayushsonirbt@gmail.com>
2022-09-10 22:20:05 +0530

    Add files via upload

---
+++ sherlock/__main__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+from platform import python_version
+import sys
+
+if __name__ == "__main__":
+    python_version = sys.version.split()[0]
+
+    if sys.version_info < (3, 6):
+        print("Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
+        sys.exit(1)
+
+    import sherlock
+    sherlock.main()
